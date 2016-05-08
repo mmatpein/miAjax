72656eb1e749bf43d241f0b3ea2abf455361ff33
Mauricio Matamala <mauriciomatamala@hotmail.com>
2016-05-08 23:13:17 +0200

    Apartados resueltos

--- colores_json.php
+++ colores_json.php
@@ -1,5 +1,18 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $consulta = file_get_contents('php://input');
+    
+    $objetoRecibido = json_decode($consulta,true);
 
+    $color = $objetoRecibido["color"];
+    
+    $db = new mysqli("localhost","colores","Perro20","Colores");
+    $resultado = $db->query("INSERT INTO colores VALUES ('".$color."');");    
+    
+    if ($resultado){
+        echo $consulta;
+    } else {
+        echo json_encode(["error" => "El color no se ha añadido"]);
+    }
 }
--- colores_xml.php
+++ colores_xml.php
@@ -8,5 +8,18 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
     
+    $color = $_GET["color"];
+    
+    $document = new DOMDocument();
+    $document->load("colores.xml");
+    
+    $colorXML = $document->createElement("color",$color);
+    
+    $raizColor = $document->getElementsByTagName("colores")->item(0);
+    $raizColor->appendChild($colorXML);
+    
+    $document->save("colores.xml");
+    echo $document->saveXML(); 
+    
     
 }
